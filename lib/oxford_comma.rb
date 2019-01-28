77fb822b014c0ad9bf533437a98fa77ceeb31fc7
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-28 23:24:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+  finalStr = ''
+  
+  i = 0
+  while i < array.length
+    current = array[i]
+    
+    if i < array.length - 1
+      finalStr += "#{current}, "
+    elsif i = array.length - 1
+      finalStr += "and #{current}"
+    end
+    
+    i += 1
+  end
+    
+  return finalStr
 end
