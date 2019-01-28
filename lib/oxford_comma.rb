cefae73d7c60a341fbe5f877c8df78783b8ac304
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-28 23:30:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   finalStr = ''
   
   if array.length == 1
-    finalStr += array[0]
+    return finalStr += array[0]
   elsif array.length == 2
-    finalStr += array[0] + "and " + array[1]
+    return finalStr += array[0] + "and " + array[1]
   else
   i = 0
   while i < array.length
